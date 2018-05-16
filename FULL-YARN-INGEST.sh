53de643bb265b1f2e9883cf8538ab5bf995a4ef5
davidb <davidb@immensity.cms.waikato.ac.nz>
2018-05-17 10:28:30 +1200

    Restructuring of script directory to emphasize that the capitilized scripts only run if CWD is the top-level directory

--- FULL-YARN-INGEST.sh
+++ FULL-YARN-INGEST.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-nohup ./scripts/FULL-RUN-YARN-SPARK.sh htrc-full-ef20 &
+nohup ./SCRIPTS-CWD/FULL-RUN-YARN-SPARK.sh htrc-full-ef20 &
 
--- JSONLIST-YARN-INGEST.sh
+++ JSONLIST-YARN-INGEST.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-nohup ./scripts/JSONLIST-RUN-YARN-SPARK.sh \
+nohup ./SCRIPTS-CWD/JSONLIST-RUN-YARN-SPARK.sh \
       pair-tree-annika-1k-fiction-vol-ids.txt TMP-faceted-htrc-fictsample-ef20 &
