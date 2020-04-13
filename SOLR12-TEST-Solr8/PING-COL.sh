337761a506281314091287c5277efef4e9807ab6
David Bainbridge <dbbridge@gsliscluster1.ischool.illinois.edu>
2020-04-13 01:05:30 -0500

    Upgraded

--- SOLR12-TEST-Solr8/PING-COL.sh
+++ SOLR12-TEST-Solr8/PING-COL.sh
@@ -3,9 +3,24 @@
 source ./_setcol.sh
 
 echo "****"
-echo "* Pinging collection:   $solrcol"
+echo "* Pinging collection:    $solrcol"
 echo "* Through Solr endpoint: $solradminurl"
 echo "****"
 
 
-wget $opt_authenticate "$solradminurl/collections?action=list&name=$solrcol" -O -
+solr_cmd="$solradminurl/collections?action=list"
+col_exists=`wget $opt_authenticate -q "$solr_cmd" -O - \
+    | python -c "import sys, json; cols=json.load(sys.stdin)['collections']; print '$solrcol' in cols" `
+
+if [ "x$col_exists" != "x" ] ; then
+    # running command produced a result
+    if [ "$col_exists" = "True" ] ; then
+	echo "#  Exists!"
+    else
+	echo "# Does NOT exist!"
+    fi
+else
+    echo "# Does NOT exist!"
+fi
+echo "****"
+
