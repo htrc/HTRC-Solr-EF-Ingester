0e5f69d7d34ff99c6f747af6275c44d7f85b1a01
David Bainbridge <dbbridge@gsliscluster1.ischool.illinois.edu>
2020-04-14 06:35:01 -0500

    Improvement to COMPILE script

--- COMPILE.sh
+++ COMPILE.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-# For standalone compilation:
-#  mvn package
+# Looks like 'mvn package' is configured (or is now!) to generate both
+# standalone and Spark cluster versions
 
-# For running on Spark cluster:
-#mvn assembly:assembly -DdescriptorId=jar-with-dependencies
+# For compiling to run run on Spark cluster:
+#   mvn assembly:assembly -DdescriptorId=jar-with-dependencies
 
 mvn package
 
+if [ $? = "0" ] ; then
+    echo ""
+    echo "****"
+    echo "* Regenerated target jar files:"
+    echo "****"
+    date
+    ls -l target/htrc-ef-ingest-*.jar
+    echo "####"
+fi
